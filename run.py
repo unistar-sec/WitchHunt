59833959cb5381f00bc09a21e893eb628a6a93a0
eastrd <yjia285@student.monash.edu>
2017-07-30 00:53:21 +1000

    Added Database interactions

--- run.py
+++ run.py
@@ -1,11 +1,40 @@
 from flask import request, render_template, Flask
-import requests
+from flask_sqlalchemy import SQLAlchemy
+import dataset
+
 
 app = Flask(__name__)
 
+db = dataset.connect("sqlite:///:memory:")
+
+table = db["Case"]
+
+
+def ScrapePage(target_url):
+    # Creates a selenium instance to fetch the webpage source
+    # Returns the webpage html given url
+    from selenium import webdriver
+    driver = webdriver.PhantomJS()
+    driver.get(target_url)
+    return driver.page_source
+
 
 def Setup(notes, url, content, time, email):
-    pass
+    # Set up the honeypot webpage given the information
+    # Return value indicates the states of setup
+    # Check if the self url already exists
+    if table.find_one(url=url) is not None:
+        return 0
+    table.insert(
+        {
+            "notes": notes,
+            "url": url,
+            "content": content,
+            "time": time,
+            "email": email
+        }
+    )
+    return 1
 
 
 @app.route("/set", methods=["GET", "POST"])
@@ -17,7 +46,7 @@ def Set_Trap():
         time = request.form["time"]
         email = request.form["email"]
         print(notes, url, content, time, email)
-        return "\n".join([notes, url, content, time, email])
+        return Setup(notes, url, content, time, email)
     else:
         return render_template("trap_set.html")
 
