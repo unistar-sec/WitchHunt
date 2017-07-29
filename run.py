fca88d7082adb77c5e6344d4428a224f0cca9fc4
eastrd <yjia285@student.monash.edu>
2017-07-29 14:26:43 +1000

    Added POST data fetch

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 from flask import request, render_template, Flask
-from time import strftime
-from os import system
 import requests
 
 app = Flask(__name__)
 
-@app.route("/set")
+
+@app.route("/set", methods=["GET", "POST"])
 def Set_Trap():
-    return render_template()
+    if request.method == "POST":
+        notes = request.form["notes"]
+        url = request.form["url"]
+        content = request.form["content"]
+        time = request.form["time"]
+        email = request.form["email"]
+        print(notes, url, content, time, email)
+        return "\n".join([notes, url, content, time, email])
+    else:
+        return render_template("trap_set.html")
 
 
 app.run(host="0.0.0.0", debug=False, port=80)
