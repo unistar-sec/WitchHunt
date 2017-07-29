8e61d0af4e2202d6feadb4c9e0a4e758a83d356d
eastrd <yjia285@student.monash.edu>
2017-07-30 01:38:54 +1000

    - Added template returning logic - Added trap trigger logic

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from flask import request, render_template, Flask
-from flask_sqlalchemy import SQLAlchemy
 import dataset
 
 
@@ -24,17 +23,43 @@ def Setup(notes, url, content, time, email):
     # Return value indicates the states of setup
     # Check if the self url already exists
     if table.find_one(url=url) is not None:
-        return 0
-    table.insert(
-        {
+        return "0"
+    else:
+        # Change content variable into the html source it's pointing to
+        if content == "500":
+            html = render_template("500.html")
+        elif content == "404":
+            html = render_template("404.html")
+        else:
+            html = ScrapePage(content)
+
+        table.insert({
             "notes": notes,
             "url": url,
-            "content": content,
+            "content": html,
             "time": time,
             "email": email
-        }
-    )
-    return 1
+        })
+    return "1"
+
+
+@app.errorhandler(404)
+def Catch(e=None):
+    suffix = "/".join(request.url.split("/")[3:])
+    print(suffix)
+    result = table.find_one(url=suffix)
+    if result is None:
+        # No trap here
+        return "FUCK OFF"
+    else:
+        # Bingo!
+        trap_url = request.url
+        email_title = result["notes"]
+        email_addr = result["email"]
+        html = result["content"]
+        valid_time = result["time"]
+        print(trap_url, email_title, email_addr, valid_time)
+        return html
 
 
 @app.route("/set", methods=["GET", "POST"])
