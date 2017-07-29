4e9be6d3064389bf646417486fd77bcb6dd1ac95
eastrd <yjia285@student.monash.edu>
2017-07-29 10:50:04 +1000

    init

--- run.py
+++ run.py
@@ -5,36 +5,9 @@ import requests
 
 app = Flask(__name__)
 
-def filter(string):
-        pass
-
-def getInfo():
-        environ = request.environ
-        shallow = request.shallow
-        access_route = request.access_route
-        charset = request.charset
-        headers = request.headers
-        remote_addr = request.remote_addr
-        remote_user = request.remote_user
-        return "Environ: %s\nshallow: %s\naccess_route: %s\ncharset: %s\nheaders: %s\nremote_addr: %s\nremote_user: %s\n" \
-        %(environ, shallow, access_route, charset, headers, remote_addr, remote_user)
-
-
-@app.errorhandler(404)
-@app.route('/')
-@app.route('/index')
-def index(e=None):
-        res = str(strftime("%m-%d %H:%M:%S"))+"\n"
-        try:
-                req = requests.get("http://ip-api.com/json/"+request.remote_addr).content
-        except Exception as e:
-                req = e
-        for i in request.environ:
-                res += str(i) + ":\t" + str(request.environ[i]) + "\n"
-        with open("potWeb.log", "a+") as f:
-                f.write(res + req + "\n"*5)
-        return render_template("500.html")
-
+@app.route("/set")
+def Set_Trap():
+    return render_template()
 
 
 app.run(host="0.0.0.0", debug=False, port=80)
